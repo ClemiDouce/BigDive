8c330ed9eb35899a5d41ee6578033acb344a6696
Mathieu Ducournau <dupycournau@gmail.com>
2021-04-26 17:08:19 +0200

    Little tweaks

--- src/Stage/TestStage.gd
+++ src/Stage/TestStage.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
-var score = 0 setget set_score
-var level = 0
+onready var mouse = $Sprite
+onready var inverse = $Inverse
 
-
-
-func _on_Timer_timeout():
-	self.score += 1
-
-func set_score(new_value):
-	score = new_value
-	$Label.text = "Score: " + str(score)
-	if score / 1000 != level:
-		level = score / 1000
-		$Label2.text = "Level: " + str(level)
+func _process(_delta):
+	mouse.position = get_global_mouse_position()
+	var mouse_pos = mouse.position
+	var middle = Vector2(80, 256/2)
+	var window = middle * 2
+	
